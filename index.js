164872b80379dac31a7788f0a05e2067de40b139
wuzhuokun <wuzhuokun@youmi.net>
2021-07-05 15:58:18 +0800

    test

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    try{
+    try {
         console.log('正在启动 Chromium')
         const browser = await puppeteer.launch({headless: false});
         const page = await browser.newPage();
-        await page.goto('https://www.baidu.com');
-    }catch (e) {
+        const url = 'https://www.baidu.com'
+        console.log('go to :' + url)
+        await page.goto(url);
+    } catch (e) {
         console.warn(e);
     }
 })();
