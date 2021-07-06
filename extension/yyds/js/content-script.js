05745444dbd0307e0f6cd51fe2a13720aac01dde
wuzhuokun <wuzhuokun@youmi.net>
2021-07-06 18:36:45 +0800

    fix

--- extension/yyds/js/content-script.js
+++ extension/yyds/js/content-script.js
@@ -1,20 +1,16 @@
 ﻿$(document).ready(function () {
     // 监听popup事件
-    const $e = new MyEvent()
-
-    chrome.runtime.onMessage.addListener((res, sender, sendResponse) => { // 接受消息
-        if (res.code === 'yyds') {
-
-            // console.log(res);
-            $e.patchEvent(100, 100)
-
-            $('body').click(e => {
-                console.log(e);
-            })
-
-            sendResponse('get')
-        }
+    // const $e = new MyEvent()
+    $('body').click(e => {
+        console.log(e);
     })
+    // chrome.runtime.onMessage.addListener((res, sender, sendResponse) => { // 接受消息
+    //     if (res.code === 'yyds') {
+    //         // console.log(res);
+    //         $e.patchEvent(100, 100)
+    //         sendResponse('get')
+    //     }
+    // })
 
 
 })
--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 const puppeteer = require('puppeteer');
+const {getPath,randomFrom} = require('./js/tools');
 
 (async () => {
     try {
         console.log('正在启动 Chromium')
-        const browser = await puppeteer.launch({headless: false});
+        const extendUrl = 'extension/yyds'
+        const options = {
+            headless: false,
+            args: [`--disable-extensions-except=${getPath(extendUrl)}`]
+        }
+        const browser = await puppeteer.launch(options);
         const page = await browser.newPage();
+
         const url = 'https://www.baidu.com'
-        console.log('go to :' + url)
         await page.goto(url);
+
+        const dimensions = await page.evaluate(() => {
+            return {
+                width: document.querySelector('#wrapper').clientWidth,
+                height: document.querySelector('#wrapper').clientHeight,
+            };
+        });
+        const {width, height} = dimensions
+        await page.setViewport({width, height})
+
+        setInterval(async ()=>{
+            await page.mouse.click(randomFrom(100,500), randomFrom(100,500));
+        },1000)
+
+        // await browser.close();
     } catch (e) {
         console.warn(e);
     }
