f401b8fe65b6754f0529c82473edd87bb49fa15f
wuzhuokun <wuzhuokun@youmi.net>
2021-08-02 15:15:26 +0800

    公共data

--- js/map.js
+++ js/map.js
@@ -63,48 +63,52 @@ const pageMap = {
     ],
     'yuling': [
         {
-        name: '御灵选择页',
-        path: 'img/yys/pages/yuhun/yuhun_out.png',
-        clickMap: [{x: 200, y: 320}],
-        clickTimes: 1,
-        simi: 0.6,
-        r: 30,
-        img: {data: null}
-    }],
+            name: '御灵选择页',
+            path: 'img/yys/pages/yuhun/yuhun_out.png',
+            clickMap: [{x: 200, y: 320}],
+            clickTimes: 1,
+            simi: 0.6,
+            r: 30,
+            img: {data: null}
+        }],
     'chi': [
         {
-        name: '痴选择页',
-        path: 'img/yys/pages/yuhun/yuhun_out.png',
-        clickMap: [{x: 200, y: 320}],
-        clickTimes: 1,
-        simi: 0.6,
-        r: 30,
-        img: {data: null}
-    }, {
-        name: '痴开始页',
-        path: 'img/yys/pages/chi/chi_start.png',
-        clickMap: [{x: 870, y: 480}, {x: 870, y: 460}, {x: 880, y: 500}],
-        clickTimes: 2,
-        simi: 0.6,
-        r: 10,
-        img: {data: null}
-    }, {
-        name: '痴结束页',
-        path: 'img/yys/pages/chi/finish.png',
-        clickMap: [{x: 700, y: 400}, {x: 430, y: 470}, {x: 500, y: 380}, {x: 650, y: 430}, {x: 140, y: 380}],
-        clickTimes: 1,
-        simi: 0.7,
-        r: 20,
-        img: {data: null}
-    }, {
-        name: '痴失败页',
-        path: 'img/yys/pages/chi/fail.png',
-        clickMap: [{x: 135, y: 385}, {x: 825, y: 400}, {x: 780, y: 175}, {x: 120, y: 170}, {x: 400, y: 160}],
-        clickTimes: 1,
-        simi: 0.7,
-        r: 20,
-        img: {data: null}
-    }]
+            name: '痴选择页',
+            path: 'img/yys/pages/yuhun/yuhun_out.png',
+            clickMap: [{x: 200, y: 320}],
+            clickTimes: 1,
+            simi: 0.6,
+            r: 30,
+            img: {data: null}
+        }, {
+            name: '痴开始页',
+            path: 'img/yys/pages/chi/chi_start.png',
+            clickMap: [{x: 870, y: 480}, {x: 870, y: 460}, {x: 880, y: 500}],
+            clickTimes: 2,
+            simi: 0.6,
+            r: 10,
+            img: {data: null}
+        }, {
+            name: '痴结束页',
+            path: 'img/yys/pages/chi/finish.png',
+            clickMap: [{x: 700, y: 400}, {x: 430, y: 470}, {x: 500, y: 380}, {x: 650, y: 430}, {x: 140, y: 380}],
+            clickTimes: 1,
+            simi: 0.7,
+            r: 20,
+            img: {data: null}
+        }, {
+            name: '痴失败页',
+            path: 'img/yys/pages/chi/fail.png',
+            clickMap: [{x: 135, y: 385}, {x: 825, y: 400}, {x: 780, y: 175}, {x: 120, y: 170}, {x: 400, y: 160}],
+            clickTimes: 1,
+            simi: 0.7,
+            r: 20,
+            img: {data: null}
+        }]
+}
+
+const globalMap = { // 公用图
+
 }
 
 function deepFreeze(obj) {
